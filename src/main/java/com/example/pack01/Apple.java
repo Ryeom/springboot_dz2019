aa1c465b94469c2f963333fdec7640e7c3b7d985
Ryeom <dmfua2@naver.com>
2019-09-18 16:35:47 +0900

    init

---
+++ src/main/java/com/example/pack01/Apple.java
@@ -0,0 +1,43 @@
+package com.example.pack01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class Apple {
+//
+////  @RequestMapping("/")
+////  public String root() {
+////    System.out.println("방법1 Apple Call");
+////    return "Apple Call";
+////  }
+//
+//	@ResponseBody
+//	@RequestMapping("/")
+//	public String root() {
+//		System.out.println("방법2 Apple Call");
+//		return "Apple Call";
+//	}
+//
+//	
+//	
+//	
+//	
+//}
+
+@Controller
+public class Apple {
+  @RequestMapping("/")
+  public String f1() { // 함수 이름을 확 바꾸었다.
+    System.out.println("곧 index.jsp 실행");
+    return "index";
+  }
+  
+  @RequestMapping("/t2")
+  public String f2() { // 함수 이름을 확 바꾸었다.
+    System.out.println("나는 t2");
+    return "t2";
+  }
+}
---
+++ src/main/webapp/WEB-INF/views/t2.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+ pageEncoding="UTF-8"%>
+<%@ page import="java.util.*, java.text.*"%>
+
+<head>
+  <title>T2</title>
+</head>
+
+<body>
+  <%=new Date()%>
+  <h2>Hello World</h2>
+  
+  
+</body>
